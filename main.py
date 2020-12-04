a507603fa0de70f50593127d011709b9f02dcb4c
Nathan Pastor <npastor24@biomedscienceacademy.org>
2020-12-04 22:09:55 +0000

    I completed the assignment and wrote all the code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 RoomArea
-Author:
+Author: Nathan Pastor
 
 Original assignment by: Edhesive Intro to CS
 
@@ -24,53 +24,60 @@ def run_tests():
   print(command)
   os.system(command)
 
-def right_triangle_area(base, height):
+def right_triangle_area(base, height): 
   """
   Given the base and height of a right triangle, returns the area.
 
-  Parameters:
+  Parameters: base(float) - base is the base of the triangle
+  height(float) - height is the height of the triangle
 
-  Returns:
+  Returns:  base_x_height(float) - area of the triangle
 
   """
+  base_x_height = float(base * height) / (2)
 
-  return 0
+  return base_x_height
 
 
 def rectangle_area(length, width):
   """
   Given the length and width of a rectangle, returns the area.
 
-  Parameters:
+  Parameters: length(float) - length is the length of the rectangle
+  width(float) - width is the width of the rectangle
 
-  Returns:
+  Returns: length_x_width(float) - area of the rectangles
 
   """
-
-  return 0
+  length_x_width = float(length * width)
+  return length_x_width
 
 
 def room_area(a, b, c, d, e):
   """
-  Given five measurements, this function calculates and returns the area of the room.
+  I broke it up as the smaller rectangle as rectangle1 and the bigger rectangle as rectangle2. I did the triangle as right_triangle.
 
-  Parameters:
+  Parameters: a, b, c, d, e (floats) - These are all the different sides of the room.
 
-  Returns:
+  Returns: rectangle1+rectangle2+triangle(float) - the area of alls the shpes added together
 
   """
+  rectangle1 = rectangle_area(d-b-e, a-c)
+  rectangle2 = rectangle_area(a, b)
+  triangle = right_triangle_area(a-c, e)
+  return rectangle1+rectangle2+triangle
 
-  return 0
 
 
 if __name__ == "__main__":
   os.system("clear") # clears the console each time you run
   
-  a = input("A: ")
-  b = input("B: ")
-  c = input("C: ")
-  d = input("D: ")
-  e = input("E: ")
+  a = float(input("A: "))
+  b = float(input("B: "))
+  c = float(input("C: "))
+  d = float(input("D: "))
+  e = float(input("E: "))
+
 
   final_area = room_area(a, b, c, d, e)
   print("Room area: " + str(final_area))
